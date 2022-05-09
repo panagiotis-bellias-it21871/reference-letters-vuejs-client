b364110ef0a75a13f6bc0d8ebb344c13b3b839ef
pan-bellias <belliaspan@gmail.com>
2022-05-09 23:03:01 +0300

    working on crud operations in UI

--- src/views/AddReferenceLetterRequest.vue
+++ src/views/AddReferenceLetterRequest.vue
@@ -1,8 +1,27 @@
 <template>
     <div class="add-reference-letter-request">
-        <div class="card mb-3">
+        <div class="row mb-3">
+            <div class="col text-right">
+                <button class="btn btn-primary" v-on:click="formShow">Create New Reference Letter Request</button>
+            </div>
+        </div>
+        <div class="card bg-dark mb-3" v-if="formDiv">
             <div class="card-body">
-                Add Reference Letter Request
+                <form>
+                    <div class="form-group">
+                      <label>Reference Letter Request Title</label>
+                      <input type="text" name="title" id="" class="form-control" placeholder="Enter request title">
+                    </div>
+                    <div class="form-group">
+                      <label>Reference Letter Request Details</label>
+                      <textarea name="body" class="form-control" placeholder="Enter request details"></textarea>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 mx-auto">
+                            <button class="btn btn-block btn-success">Create Request</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -10,7 +29,17 @@
 
 <script>
 export default {
-    name: "AddReferenceLetterRequest"
+    name: "AddReferenceLetterRequest",
+    data(){
+        return {
+            formDiv: false
+        }
+    },
+    methods: {
+        formShow(){
+            this.formDiv = true;
+        }
+    }
 }
 </script>
 
--- src/views/ReferenceLetterRequestDetails.vue
+++ src/views/ReferenceLetterRequestDetails.vue
@@ -1,12 +1,42 @@
 <template>
     <div class="reference-letter-request-details">
-        Reference Letter Request Details
+        <div class="row mb-3">
+            <div class="col-auto">
+                <router-link class="btn btn-clock btn-info" to="/">Go Back</router-link>
+            </div>
+        </div>
+        <div class="card bg-dark mb-3">
+            <div class="card-body">
+                <h3>{{todo.title}}</h3>
+                <p>{{todo.completed}}</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    name: "ReferenceLetterRequestDetails"
+    name: "ReferenceLetterRequestDetails",
+    data() {
+        return {
+            id: this.$route.params.id,
+            todo: []
+        }
+    },
+    created(){
+        axios
+            .get(`https://jsonplaceholder.typicode.com/todos/${this.id}`)
+            //process.env.VUE_APP_BACKEND_URL
+            .then(res => {
+                this.todo = res.data
+                //this.rl_request = res.data
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
+    }
 }
 </script>
 
