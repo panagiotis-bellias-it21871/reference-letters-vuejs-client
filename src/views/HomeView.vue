c51b939d2f27bc2dff41d4f793dd62e048b912d3
pan-bellias <belliaspan@gmail.com>
2022-05-09 21:31:21 +0300

    working with axios and api calls

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,14 @@
   <div class="home">
     <img alt="Reference Letter Logo" src="../assets/logo.jpg">
     <AddReferenceLetterRequest />
-    <ReferenceLetterRequestList />
+    <ReferenceLetterRequestList v-bind:todos="todos"/> <!-- v-bind:rl_requests="rl_requests" -->
   </div>
 </template>
 
 <script>
+
+import axios from "axios";
+
 // @ is an alias to /src
 import AddReferenceLetterRequest from "@/views/AddReferenceLetterRequest.vue"
 import ReferenceLetterRequestList from "@/views/ReferenceLetterRequestList.vue"
@@ -16,6 +19,24 @@ export default {
   components: {
     AddReferenceLetterRequest,
     ReferenceLetterRequestList
+  },
+  data(){
+    return {
+      todos: [],
+      // rl_requests: [],
+      errors: []
+    }
+  },
+  created(){
+    axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+    //process.env.VUE_APP_BACKEND_URL
+    .then(res => {
+      this.todos = res.data
+      //this.rl_requests = res.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
   }
 }
 </script>
--- src/views/ReferenceLetterRequestList.vue
+++ src/views/ReferenceLetterRequestList.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="reference-letter-request-list">
-        <div class="card">
+        <div class="card mb-3" v-for="todo of todos" :key="todo.id"> <!-- v-for="rl_request of rl_requests"
+        :key="rl_request.id" -->
             <div class="card-body">
-                Reference Letter Request List
+                <h3><router-link :to="`/reference-letter-request-details/${todo.id}`">{{todo.title}}</router-link></h3>
+                <p>{{todo.completed}}</p>
+            </div>
+        </div>
+        <div class="card mb-3" v-if="errors && errors.length">
+            <div class="card-body">
+                <p v-for="(error, index) of errors" :key="index">{{error.message}}</p>
             </div>
         </div>
     </div>
@@ -10,7 +17,8 @@
 
 <script>
 export default {
-    name: "ReferenceLetterRequestList"
+    name: "ReferenceLetterRequestList",
+    props: ["todos"] // rl_requests
 }
 </script>
 
